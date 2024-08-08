7bb3afe2a3fd8db2b3d5e9cb4335638989f1fb12
guilopes2020 <guilopesdev@gmail.com>
2024-08-08 19:12:36 -0300

    feat: :sparkles: concept-of-data-binding

--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,4 +1,9 @@
-<div>
-    <h1>Olá {{$name}}</h1>
-    <p>{{$randonNamber}}</p>
+<div style="background-color: {{$color}}; border: 1px solid red">
+    <h1>{{$salutation}} {{ strtoupper($name) }}</h1>
+    <select wire:model.live="salutation">
+        @foreach ($salutationOptions as $option)
+            <option value="{{$option}}">{{$option}}</option>
+        @endforeach
+    </select>
+    <input type="color" wire:model.live="color">
 </div>
